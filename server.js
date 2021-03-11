54b8d2baf5b9a345592771f38741eada3e916ba4
IlayTissona <ilay211@gmail.com>
2021-03-11 17:48:36 +0000

    first POST endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const User = require("./mongo");
 
-app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -12,7 +12,14 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/exercise/new-user", (req, res) => {
-  console.log(req);
+  const {username} = req.body;
+  let newUser = new User({username})
+  console.log(newUser);
+  newUser.save().then((saved)=>{
+    console.log(saved);
+    res.json(saved);
+  }).catch(e=>{res.sendStatus(500)});
+
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
